a93ff7783ae1378f84356651f9813aadc61b77c3
yourPhoebus <evgenich054@gmail.com>
2024-05-01 15:42:53 +0300

    rel 0.2.4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
-import TeaPage from "./components/pages/TeaPage";
+// import TeaPage from "./components/pages/TeaPage";
 import AdminPage from "./components/pages/AdminPage";
-import UserPage from "./components/pages/UserPage";
+// import UserPage from "./components/pages/UserPage";
 import Root from "./components/Root";
 
 function App() {
@@ -17,18 +17,18 @@ function App() {
           path: "/",
           element: <MainPage />,
         },
-        {
-          path: "/teas/:id",
-          element: <TeaPage />,
-        },
+        // {
+        //   path: "/teas/:id",
+        //   element: <TeaPage />,
+        // },
         {
           path: "/adminpage",
           element: <AdminPage />,
         },
-        {
-          path: "/user",
-          element: <UserPage />,
-        },
+        // {
+        //   path: "/user",
+        //   element: <UserPage />,
+        // },
       ],
     },
   ]);
