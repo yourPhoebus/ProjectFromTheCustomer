636935ac479c2768f51683140aaf4b53b4ece12f
yourPhoebus <evgenich054@gmail.com>
2024-05-01 13:59:20 +0300

    rel 0.2.3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./components/pages/MainPage";
+import TeaPage from "./components/pages/TeaPage";
+import AdminPage from "./components/pages/AdminPage";
+import UserPage from "./components/pages/UserPage";
+import Root from "./components/Root";
+
 function App() {
 
-  return 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <MainPage />,
+        },
+        {
+          path: "/teas/:id",
+          element: <TeaPage />,
+        },
+        {
+          path: "/adminpage",
+          element: <AdminPage />,
+        },
+        {
+          path: "/user",
+          element: <UserPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
