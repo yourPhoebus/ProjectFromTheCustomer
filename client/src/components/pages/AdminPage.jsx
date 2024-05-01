24e41455148e868ea7451bfbdd0e78087bfe8282
yourPhoebus <evgenich054@gmail.com>
2024-05-01 14:55:23 +0300

    rel 0.2.4

---
+++ client/src/components/pages/AdminPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  Container  from 'react-bootstrap'
+
+export default function AdminPage() {
+  return (
+    <Container>
+        <h1>Admin Page</h1>
+    </Container>
+  )
+}
