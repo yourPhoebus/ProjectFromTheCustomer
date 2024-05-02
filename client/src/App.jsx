2fe5a804670651fa97a4f9df5b0519e09a5d43eb
belek1233 <belek12333@gmail.com>
2024-05-02 19:37:11 +0300

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,109 +1,36 @@
-import {
-  createBrowserRouter, RouterProvider, redirect, Link, useNavigate,
-} from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import axiosInstance, { setAccessToken } from './axiosInstance';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from './components/pages/MainPage';
-import TeaPage from "./components/pages/TeaPage";
-import AdminPage from "./components/pages/AdminPage";
-// import UserPage from "./components/pages/UserPage";
-import Root from './components/Root';
-import UserPage from './components/pages/UserPage';
+import TeaPage from './components/pages/TeaPage';
 import AdminPage from './components/pages/AdminPage';
-import SignupPage from './components/pages/SignupPage';
-import LoginPage from './components/pages/LoginPage';
-import ProtectedRoute from './components/hoc/ProtectedRoute';
+import UserPage from './components/pages/UserPage';
+import Root from './components/Root';
 
 function App() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    axiosInstance('/tokens/refresh').then((res) => {
-      const { user: newUser, accessToken } = res.data;
-      setUser(newUser);
-      setAccessToken(accessToken);
-    }).catch(() => {
-      setUser(null);
-    });
-  }, []);
-
-  const loginHandler = async (event) => {
-    event.preventDefault();
-    const formData = Object.fromEntries(new FormData(event.target));
-    const res = await axiosInstance.post('/auth/login', formData);
-    const { data } = res;
-    setUser(data.user);
-    setAccessToken(data.accessToken);
-  };
-
-  const signupHandler = async (formData) => {
-    const res = await axiosInstance.post('/auth/signup', formData);
-    const { data } = res;
-    setUser(data.user);
-    setAccessToken(data.accessToken);
-  };
-
-  const logoutHandler = async () => {
-    await axiosInstance('/auth/logout');
-    setUser(null);
-    setAccessToken('');
-  };
-
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Root user={user} logoutHandler={logoutHandler} />,
+      element: <Root />,
       children: [
         {
           path: '/',
           element: <MainPage />,
         },
         {
-          path: '/adminpage',
-          element: <AdminPage user={user} />,
-        },
-        {
-          path: '/userpage',
-          element: <UserPage user={user} />,
-        },
-        {
-          element: <ProtectedRoute isAllowed={!user} />,
-          children: [
-            {
-              path: '/login',
-              element: <LoginPage loginHandler={loginHandler} />,
-            },
-            {
-              path: '/signup',
-              element: <SignupPage signupHandler={signupHandler} />,
-            },
-          ],
-        },
-        // {
-        //   path: '/login',
-        //   element: <LoginPage loginHandler={loginHandler} />,
-        // },
-        // {
-        //   path: '/signup',
-        //   element: <SignupPage signupHandler={signupHandler} />,
-        // },
-        {
-          path: "/teas/:id",
+          path: '/teas/:id',
           element: <TeaPage />,
         },
         {
-          path: "/adminpage",
+          path: '/adminpage',
           element: <AdminPage />,
         },
-        // {
-        //   path: "/user",
-        //   element: <UserPage />,
-        // },
+        {
+          path: '/userpage',
+          element: <UserPage />,
+        },
       ],
     },
   ]);
-
   return <RouterProvider router={router} />;
 }
-
 export default App;
