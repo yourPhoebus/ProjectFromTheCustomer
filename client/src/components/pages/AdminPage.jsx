1823bc4c829ab533ba7caaa214f8ff280069f491
yourPhoebus <evgenich054@gmail.com>
2024-05-02 17:59:02 +0300

    rel 1.1

--- client/src/components/pages/AdminPage.jsx
+++ client/src/components/pages/AdminPage.jsx
@@ -1,31 +1,72 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
-import Container from 'react-bootstrap/esm/Container'
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Col, Row, Stack } from 'react-bootstrap';
+import axios from 'axios';
 import APForm from '../ui/APForm';
 import APSelect from '../ui/APSelect';
+import TeaCard from '../ui/TeaCard';
 
 export default function AdminPage() {
   const [show, setShow] = useState(false);
-
+  const [teas, setTeas] = useState([]);
+  useEffect(() => {
+    fetch('/api/tea')
+      .then((res) => res.json())
+      .then((data) => setTeas(data));
+  }, []);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [input, setInput] = useState({
+    sort: '', name: '', description: '', location: '', country: '', img: '',
+  });
+  const inputHandler = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.post('/api/tea', input).then((res) => {
+      setTeas((prev) => [res.data, ...prev]);
+      setInput({
+        sort: '', name: '', description: '', location: '', country: '', img: '',
+      });
+    });
+  };
+
+  const deleteHandler = async (id) => {
+    try {
+      await axios.delete(`/api/tea/${id}`);
+      setTeas((prev) => prev.filter((el) => el.id !== id));
+    } catch (error) {
+      console.error('Error deleting card:', error);
+    }
+  };
   return (
-    <Container style={{display:"flex"}}>
-      <Button variant="primary" onClick={handleShow}>
-        Добавление
-      </Button>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Добавление</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <APForm />
-        </Offcanvas.Body>
-      </Offcanvas>
-      <APSelect />
-    </Container>
-  
-  )
+    <>
+      <Stack direction="horizontal" gap={3}>
+        <Button variant="primary" onClick={handleShow}>
+          Добавление
+        </Button>
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Добавление</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <APForm input={input} submitHandler={submitHandler} inputHandler={inputHandler} />
+          </Offcanvas.Body>
+        </Offcanvas>
+        <div className="p-2 ms-auto">
+          <APSelect />
+        </div>
+      </Stack>
+
+      <Row>
+        {teas?.map((tea) => (
+          <Col xs={4}><TeaCard key={tea.id} deleteHandler={deleteHandler} tea={tea} /></Col>
+        ))}
+      </Row>
+    </>
+  );
 }
--- client/src/components/ui/APForm.jsx
+++ client/src/components/ui/APForm.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import  Form  from 'react-bootstrap/Form'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export default function APForm() {
+export default function APForm({ input, inputHandler, submitHandler }) {
   return (
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Сорт</Form.Label>
-      <Form.Control name='sort' type="text" placeholder="Сорт" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Название</Form.Label>
-      <Form.Control name='name' type="text" placeholder="Название" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Описание</Form.Label>
-      <Form.Control name='description' type="text" placeholder="Описание" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Гео позиция</Form.Label>
-      <Form.Control name='location' type="text" placeholder="Гео позиция" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Страна</Form.Label>
-      <Form.Control name='country' type="text" placeholder="Страна" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>img</Form.Label>
-      <Form.Control name='img' type="text" placeholder="img" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-  )
+    <Form onSubmit={submitHandler}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Сорт</Form.Label>
+        <Form.Control name="sort" onChange={inputHandler} value={input.sort} type="text" placeholder="Сорт" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Название</Form.Label>
+        <Form.Control name="name" onChange={inputHandler} value={input.name} type="text" placeholder="Название" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Описание</Form.Label>
+        <Form.Control name="description" onChange={inputHandler} value={input.description} type="text" placeholder="Описание" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Гео позиция</Form.Label>
+        <Form.Control name="location" onChange={inputHandler} value={input.location} type="text" placeholder="Гео позиция" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Страна</Form.Label>
+        <Form.Control name="country" onChange={inputHandler} value={input.country} type="text" placeholder="Страна" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>img</Form.Label>
+        <Form.Control name="img" onChange={inputHandler} value={input.img} type="text" placeholder="img" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
